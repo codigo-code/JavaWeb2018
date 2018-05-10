f54e7a8ceef126ce4032c9c1b4423c889d7c4202
Dante Panella <dpanella@usersad.everis.int>
2018-05-10 20:44:00 -0300

    update

--- ReservaApi/src/main/java/com/utn/service/ReservaBarApi.java
+++ ReservaApi/src/main/java/com/utn/service/ReservaBarApi.java
@@ -1,7 +1,11 @@
 package com.utn.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
@@ -17,8 +21,16 @@ public class ReservaBarApi {
 	private Gson gson;
 	
 	@GetMapping(value="getBar")
-	public String getBar() {
+	public String getAllBar() {
 		
-	 return gson.toJson(reservaBar.findById(1));
+	 return gson.toJson(reservaBar.findAll());
+	}
+	
+	
+	@CrossOrigin
+	@GetMapping(value="getBar/{id}")
+	public ResponseEntity getBarById(@PathVariable("id") Integer id) {
+		
+	 return ResponseEntity.ok(gson.toJson(reservaBar.findById(id)));
 	}
 }
